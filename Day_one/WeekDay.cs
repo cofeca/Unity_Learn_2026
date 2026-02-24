b6de0db47e0035e8bf78fda4e061512b9e0b8b5e
cofeca <2455152485@qq.com>
2026-02-24 11:33:11 +0800

    Day01 20260224 C#基础：switch判断

---
+++ Day_one/WeekDay.cs
@@ -0,0 +1,79 @@
+﻿using System;
+//定义 int 变量 week（1-7）；
+//switch 判断：1 = 周一，2 = 周二…7 = 周日，输出对应文字。
+namespace Game
+{
+    class WeekDay
+    {
+        int week;
+        public void SetWeek()
+        {
+            week = 2;
+        }
+        public string TodayIs()
+        {
+            switch (week)
+            {
+                case 1:
+                    return "今天是星期一";
+   
+                case 2:
+                    return "今天是星期二";
+                    
+                case 3:
+                    return "今天是星期三";
+                    
+                case 4:
+                    return "今天是星期四";
+                    
+                case 5:
+                    return "今天是星期五";
+                    
+                case 6:
+                    return "今天是星期六";
+                    
+                case 7:
+                    return "今天是星期日";
+                    
+                default:
+                    return "数据错误";
+                    
+            }
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            WeekDay weekDay = new WeekDay();
+            weekDay.SetWeek();
+            Console.WriteLine(weekDay.TodayIs());
+            Console.ReadLine();
+        }
+
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
